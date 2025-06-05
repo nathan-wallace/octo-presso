8e73c741d96180028d803a2250d5a205c2c23b0a
Nathan Wallace <nathan.s.wallace@gmail.com>
2025-06-05 17:35:44 -0400

    introduced shape concept

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -17,9 +17,6 @@
                        <li aria-current={page.url.pathname === base || page.url.pathname === base + '/' ? 'page' : undefined}>
                                <a href={base || '/'}>Home</a>
                        </li>
-                       <li aria-current={page.url.pathname.startsWith(`${base}/about`) ? 'page' : undefined}>
-                               <a href={`${base}/about`}>About</a>
-                       </li>
                        <li aria-current={page.url.pathname.startsWith(`${base}/screensaver`) ? 'page' : undefined}>
                                <a href={`${base}/screensaver`}>Screensaver</a>
                        </li>
