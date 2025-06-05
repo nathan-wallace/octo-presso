a0a723e3a65f793f8e8566f023184f7ea5ed6750
Nathan Wallace <nathan.s.wallace@gmail.com>
2025-06-05 19:09:21 -0400

    test commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV === 'development';
-
 export default {
   kit: {
     adapter: adapter({
-      pages: 'docs',   // ← replaces default "build"
+      pages: 'docs',
       assets: 'docs',
       fallback: null
     }),
     paths: {
-      base: dev ? '' : '/octo-presso',    // optional: for GitHub Pages or subdir deploys
+      base: '/octo-presso',
     }
   }
 };
