a42fa8089b5c5120b48eac8f18a2583d2eb40ba2
nathan-wallace <nathan.s.wallace@gmail.com>
2025-06-05 20:49:01 -0400

    feat: dynamic page listing

--- src/lib/pages.js
+++ src/lib/pages.js
@@ -6,3 +6,14 @@ export const marketingPages = Object.keys(
   const name = slug.charAt(0).toUpperCase() + slug.slice(1);
   return { slug, name };
 });
+
+export const appPages = Object.keys(
+  import.meta.glob('/src/routes/*/+page.svelte')
+)
+  .filter((file) => !file.includes('[slug]') && file !== '/src/routes/+page.svelte')
+  .map((file) => {
+    const parts = file.split('/');
+    const slug = parts[parts.length - 2];
+    const name = slug.charAt(0).toUpperCase() + slug.slice(1);
+    return { slug, name };
+  });
